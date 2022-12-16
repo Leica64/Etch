0d2f53d430e03642e4b0fb4f0a3840f0e7e0d59e
Leica64 <hehmann.alice@gmail.com>
2022-12-15 22:25:06 -0500

    box go in boat

--- magic.js
+++ magic.js
@@ -1,15 +1,32 @@
 
 function makeNewDiv() {
    let input = prompt('grid size');
-   while (input){ 
-     const element = document.createElement('div');
-     element.setAttribute(
-      'style',
-      'background-color: red; width: 20px; height: 20px;' 
-     );
-     console.log(input);
-      const box = document.getElementById('box');
-      box.appendChild(element);
-     input--;
+   let shlongus = parseInt(input,10);
+   let gridSize = shlongus * shlongus;
+   let rowOfBoxes = [];
+   const ship = document.getElementsByClassName("ship");
+
+   while (gridSize){ 
+     let element = document.createElement('div');
+     element.setAttribute('id','box');
+     rowOfBoxes.push(element);
+      if ((gridSize -1) % shlongus === 0){
+       const container = document.createElement('div');
+       container.setAttribute('class','container');
+       rowOfBoxes.forEach(box => {
+        container[0].appendChild(box);
+       });
+       ship[0].appendChild(container);
+       rowOfBoxes = [];
+     }
+     gridSize--;
     }
+}
+
+function clearGrid(){
+  const parent = document.getElementsByClassName("box");
+while (parent.firstChild) {
+    parent.firstChild.remove();
+  }
+  console.log("Cleared");
 }
